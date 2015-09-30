bc9c1b959b085ee87b138e74b575b0448597a534
Amr Mohamed <AmrMohamed@yahoo.com>
2015-09-30 16:46:24 +0200

    finish UI

--- public/apps/ui/js/controllers/amrRatingController.js
+++ public/apps/ui/js/controllers/amrRatingController.js
@@ -1,11 +1,29 @@
 (function (module) {
     var amrRatingController = function ($scope) {
-        this.initialize = function () {
-
+        this.initialize = function (min, max) {
+            $scope.stars = Array(max - min + 1);
+            $scope.preview = -1;
         };
 
-        $scope.click = function () {
+        $scope.click = function ($index) {
+            $scope.value = $index +1;
+        };
 
+        $scope.mouseover = function ($index) {
+            $scope.preview = $index;
+        };
+
+        $scope.mouseout = function () {
+            $scope.preview = -1;
+        };
+
+        $scope.styles = function ($index) {
+            return {
+                "glyphicon": true,
+                "glyphicon-star": $index < $scope.value,
+                "glyphicon-star-empty": $index >= $scope.value,
+                "starpreview": $index <= $scope.preview
+            };
         };
     };
 
--- public/apps/ui/js/directives/rating2.js
+++ public/apps/ui/js/directives/rating2.js
@@ -7,35 +7,14 @@
                 value : '='
             },
             require : 'amrRating',
-            templateUrl : 'templates/rating',
+            templateUrl : 'templates/rating.html',
             controller : "amrRatingController",
             link : function (scope, element, attributes, controller) {
 
                 var min = parseInt(attributes.min  || "1");
                 var max = parseInt(attributes.max  || "10");
 
-                element.addClass("btn-group");
-
-                scope.$watch('value', function(newValue) {
-                    element.empty();
-                    console.log(newValue);
-                    for(var i = 0; i < newValue; i++) {
-                        element.append("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-star'></span></button>");
-                    }
-                });
-                element.on('click', function () {
-                    scope.$apply(function () {
-                        var rateValue = parseInt(scope.value);
-                        if (rateValue < max)  {
-                            rateValue += 1;
-                            scope.value = rateValue;
-                        }
-                        else
-                            scope.value = min;
-                    });
-
-                });
-
+                controller.initialize(min, max);
             }
         };
 
